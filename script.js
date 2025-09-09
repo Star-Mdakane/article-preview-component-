1560535d45bf53d2d3c278f05210d0ef365bde58
Star <mmdakane@rocketmail.com>
2025-09-09 23:30:30 +0200

    final commit

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const modalBtn = document.querySelector("#modal-btn");
 const container = document.querySelector("#container")
 
 avatarBtn.addEventListener("click", (e) => {
-    container.classList.add("open");
+    if (!container.classList.contains("open")) {
+        container.classList.add("open");
+    } else {
+        container.classList.remove("open");
+    }
 });
 
 modalBtn.addEventListener("click", () => {
